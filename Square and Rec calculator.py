d1f24fcf15b8c82dd385dde3a13c4fda38a83e4b
ProTech <71920685+ProTech2206@users.noreply.github.com>
2020-09-29 11:12:57 +0530

    Add files via upload

---
+++ Square and Rec calculator.py
@@ -0,0 +1,14 @@
+print('SQUARE OR RECTANGULAR?')
+print('Tᴏᴏʟ Oᴡɴᴇʀ: PʀᴏTᴇᴄʜ2206')
+print('Sᴄʀɪᴘᴛ ᴍᴀᴅᴇ ʙʏ: Sɪᴀ')
+print('Eᴅɪᴛᴇᴅ ʙʏ: AᴍᴀᴛᴜᴇʀCᴏᴅᴇʀ')
+print('YᴏᴜTᴜʙᴇ: AᴍᴀᴛᴜᴇʀCᴏᴅᴇʀ PʀᴏTᴇᴄʜ')
+print('      ')
+print('|______________________|')
+print('              ')
+l=int(input('Enter length:'))
+b=int(input('Enter Breadth:'))
+if l==b:
+   print('Yes, it is square')
+else:
+    print('No, it is a Rectangle')
